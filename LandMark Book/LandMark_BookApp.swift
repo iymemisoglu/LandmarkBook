52832168768b785fb366fa1a78eb205fde480e48
ilker <ilker.memisoglu@icloud.com>
2023-01-26 21:22:16 +0300

    Initial Commit

---
+++ LandMark Book/LandMark_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMark_BookApp.swift
+//  LandMark Book
+//
+//  Created by İlker Yasin Memişoğlu on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandMark_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
